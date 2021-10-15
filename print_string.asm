683449b970de3c7fe66957ae48c6536e06069c95
Abdul Aziz <abdul20485@iiitd.ac.in>
2021-10-16 02:28:59 +0530

    2

--- print_string.asm
+++ print_string.asm
@@ -17,10 +17,25 @@ _start:
 
 _print:
 	push rax
+
 	mov rbx,0
+	;initialized i=0
+
 _printLoop:
+
 	inc rax
+	;moving in character of string i.e h to e to l
 	inc rbx
+	;increasing i
+	mov cl,[rax]
+	;cl is a 8 bit regiter i.e can store only 1 register lol abobve command will move the value of rax in cl eg if we have "xyz" then cl has x so we w ereach 0 after new linw
+
+
+
+
+
+
+
 	mov cl, [rax]
 	cmp cl, 0
 	jne _printLoop
